9f9c80e8c9914cc1b4dcc21471e9ccb5e2a16909
sherifalafifioo1 <147742240+sherifalafifioo1@users.noreply.github.com>
2024-06-22 09:00:02 +0300

    Update ID_version2.py

--- ID_version2.py
+++ ID_version2.py
@@ -14,35 +14,39 @@ import numpy as np
 
 app = Flask(__name__)
 
+
 @app.route('/predict_image', methods=['POST'])
 def predict_image():
 
 
-   def OCR_pipeline(img_path=''):
-    
-           gray = img_to_gray(img_path)
+    def OCR_pipeline(img_path=''):
 
-       # Get homography between Gomhoreyet Masr template and ID
+        gray = img_to_gray(img_path)
+
+        # Get homography between Gomhoreyet Masr template and ID
         M, match_boundingbox = findOrientation(gray)
 
         # If matches found Apply Orientation
-    if M is not None:
+        if M is not None:
             gray, img, M = applyOrientation(gray, img, M)
-    
-            # blur gray for smoothing the image for the edge detection
-            gray = blur_img(gray)
-            # Adaptive threshold
-            bin = adaptive_threshold(gray,11, 2)
-            # Edge detection
-            edges = canny_edge_detector(bin, 200, 600)
 
-    
-            # Getting largest Contour
-            contour_bbox, largest_contour = findLargestContour(edges)
+        # blur gray for smoothing the image for the edge detection
+        gray = blur_img(gray)
+
+        # Adaptive threshold
+        bin = adaptive_threshold(gray, 11, 2)
+
+        # Edge detection
+        edges = canny_edge_detector(bin, 200, 600)
+
+
+        # Getting largest Contour
+        contour_bbox, largest_contour = findLargestContour(edges)
         if contour_bbox is not None:
 
-            cropped_img = output_img(img, largest_contour) 
+            cropped_img = output_img(img, largest_contour)
             cropped_gray = img_to_gray(cropped_img)
+
             ##output_pth = 'C:\\Users\\USER\\Documents\\Grad_project\\Authentication\\Authentication_Grad_Project\\output\\'
 
             selections, bboxes = detect_text_regions(cropped_gray)
@@ -50,23 +54,22 @@ def predict_image():
                 x, y, x2, y2 = bboxes[i]
                 text_img = cropped_img[y:y2, x:x2]
                 text_bin = preproccess_OCR(text_img)
-                id_str = OCR_pytesseract(text_bin).replace(" ","").replace("\n","").rstrip()
+                id_str = OCR_pytesseract(text_bin).replace(" ", "").replace("\n", "").rstrip()
                 if len(id_str) != 14:
                     continue
                 National_ID = int(id_str)
-                return(National_ID)
-          else:
-                    print("No contours")
-      else:
-                print("No ID Card Found / No contours")
+                return (National_ID)
+        else:
+            print("No contours")
+    else:
+        print("No ID Card Found / No contours")
 
 
-    
     try:
         # Check if images are present and valid
         if 'image1' not in request.files or 'image2' not in request.files:
-            return jsonify({"status": 400, "msg": "Missing one or both images (image1, image2)"}), 400 
-    
+            return jsonify({"status": 400, "msg": "Missing one or both images (image1, image2)"}), 400
+
         # Read images from form data and convert to NumPy arrays
         image1_file = request.files['image1']
         image1_data = np.frombuffer(image1_file.read(), np.uint8)
@@ -77,7 +80,8 @@ def predict_image():
         image2_array = cv2.imdecode(image2_data, cv2.IMREAD_COLOR)
 
         # Process images
-        id=OCR_pipline(image1_array)
+        id = OCR_pipeline(image1_array)
+
         # Return response
         return jsonify({
             "status": 200,
@@ -89,26 +93,6 @@ def predict_image():
         # Handle errors during processing
         return jsonify({"status": 500, "error": str(e)}), 500
 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, threaded=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-            
-
-
-#******************************************************
-
-
-        
-
