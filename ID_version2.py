cddcd9ba9e19e174c4f51dcda1ceb76693b55957
sherifalafifioo1 <147742240+sherifalafifioo1@users.noreply.github.com>
2024-06-22 08:52:17 +0300

    Update ID_version2.py

--- ID_version2.py
+++ ID_version2.py
@@ -18,38 +18,27 @@ app = Flask(__name__)
 def predict_image():
 
 
-    def OCR_pipeline(img_path=''):
-    """from ID img to ID number through applying contours and wrap prespective , cropping the image , thresholding and finally applying OCR
-
-    Args:
-        img_path(string): contains image path , defaults to ''
-        
-    Raises:
-        Exception: if ID_number are not  14 digit
-
-    Returns:
-        int: ID number
-        """
+   def OCR_pipeline(img_path=''):
     
-    gray = img_to_gray(img_path)
+           gray = img_to_gray(img_path)
 
-    # Get homography between Gomhoreyet Masr template and ID
-    M, match_boundingbox = findOrientation(gray)
+       # Get homography between Gomhoreyet Masr template and ID
+        M, match_boundingbox = findOrientation(gray)
 
-    # If matches found Apply Orientation
+        # If matches found Apply Orientation
     if M is not None:
-        gray, img, M = applyOrientation(gray, img, M)
+            gray, img, M = applyOrientation(gray, img, M)
+    
+            # blur gray for smoothing the image for the edge detection
+            gray = blur_img(gray)
+            # Adaptive threshold
+            bin = adaptive_threshold(gray,11, 2)
+            # Edge detection
+            edges = canny_edge_detector(bin, 200, 600)
 
-        # blur gray for smoothing the image for the edge detection
-        gray = blur_img(gray)
-        # Adaptive threshold
-        bin = adaptive_threshold(gray,11, 2)
-        # Edge detection
-        edges = canny_edge_detector(bin, 200, 600)
-
-
-        # Getting largest Contour
-        contour_bbox, largest_contour = findLargestContour(edges)
+    
+            # Getting largest Contour
+            contour_bbox, largest_contour = findLargestContour(edges)
         if contour_bbox is not None:
 
             cropped_img = output_img(img, largest_contour) 
@@ -66,9 +55,9 @@ def predict_image():
                     continue
                 National_ID = int(id_str)
                 return(National_ID)
-        else:
+          else:
                     print("No contours")
-    else:
+      else:
                 print("No ID Card Found / No contours")
 
 
